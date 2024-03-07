4344303eaadafbd4ba5113b39e54b5f6f23d1d1a
Mark <m.matthewman@gmail.com>
2024-03-07 15:09:19 +0100

    Added new features

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -11,19 +11,27 @@ import matplotlib.pyplot as plt
 import mplhep as hep
 import pickle
 
+SHAPEVARIABLENAMES = ["EV1", "EV2","EV3","eVector0_x","eVector0_y","eVector0_z","sigmaPCA1","sigmaPCA2","sigmaPCA3","barycenter_x","barycenter_y","barycenter_z"]
+FEATURENAMESSUM = ['cp_energy', 'tkx_energy', 'cp_tkx_energy_frac', 'tkx_numtkx', 'barycenter_x', 'barycenter_y', 'barycenter_z','cee_120', 'cee_200', 'cee_300', 'ceh_120', 'ceh_200', 'ceh_300', 'ceh_scint']
+FEATURENAMESMAX = ['cp_energy', 'tkx_energy', 'cp_tkx_energy_frac', 'tkx_numtkx','cee_120', 'cee_200', 'cee_300', 'ceh_120', 'ceh_200', 'ceh_300', 'ceh_scint'] + SHAPEVARIABLENAMES
+
+def getMaxShapeVariable(f,key,argmax_idx):
+    return [x[l] for x,l in zip(f["ticlDumper/tracksters"][key].array(library="np"),argmax_idx)]
+
+def filterMaxShapeVariables(var,f_idx):
+    return [i for j, i in enumerate(var) if j not in f_idx]
 
 class Preprocessor():
     def __init__(self,path,mode="Max"):
-        self.feature_names = [
-        'cp_energy', 'tkx_energy', 'cp_tkx_energy_frac', 'tkx_numtkx', 
-        'weighted_bar_x', 'weighted_bar_y', 'weighted_bar_z',
-        'cee_120', 'cee_200', 'cee_300', 'ceh_120', 'ceh_200', 'ceh_300', 'ceh_scint']
-        
-        self.initializeFeatures()
+
         #self.fillFeaturesSummedTrackster(path)
         if mode == "Sum":
+            self.feature_names = FEATURENAMESSUM
+            self.initializeFeatures()
             self.fillFeaturesSummedTrackster(path)
         elif mode == "Max":
+            self.feature_names = FEATURENAMESMAX
+            self.initializeFeatures()
             self.fillFeaturesMaxTrackster(path)
 
 
@@ -73,16 +81,14 @@ class Preprocessor():
                     f2_idx = np.where(cp_energy<10)        # Exclude CPs with energies less than 10 GeV
                     f_idx = np.union1d(f1_idx,f2_idx)   
                     
-
                     # Write quantities
-
                     self.cp_energy += np.delete(cp_energy, f_idx).tolist()
                     self.tkx_energy +=np.delete(tkx_energy,f_idx).tolist() 
                     self.cp_tkx_energy_frac += np.delete(tkx_energy/cp_energy,f_idx).tolist() 
                     self.tkx_numtkx += np.delete(tkx_numtkx,f_idx).tolist()
-                    self.weighted_bar_x += np.delete(weighted_bar_x,f_idx).tolist() 
-                    self.weighted_bar_y += np.delete(weighted_bar_y,f_idx).tolist() 
-                    self.weighted_bar_z += np.delete(weighted_bar_z,f_idx).tolist()
+                    self.bar_x += np.delete(weighted_bar_x,f_idx).tolist() 
+                    self.bar_y += np.delete(weighted_bar_y,f_idx).tolist() 
+                    self.bar_z += np.delete(weighted_bar_z,f_idx).tolist()
                     # Split Energy per Cell
                     cell_types = [i for j, i in enumerate(cell_types) if j not in f_idx]
                     cell_types = np.array(cell_types)
@@ -116,11 +122,11 @@ class Preprocessor():
                     # Number of Tracksters
                     tkx_numtkx =np.array([ak.count(x) for x in f["ticlDumper/tracksters"]["raw_energy"].array()])
 
-                    # Barycenter
-                    bar_x = [x[l] for x,l in zip(f["ticlDumper/tracksters"]["barycenter_x"].array(library="np"),argmax_idx)]
-                    bar_y = [x[l] for x,l in zip(f["ticlDumper/tracksters"]["barycenter_y"].array(library="np"),argmax_idx)]
-                    bar_z = [x[l] for x,l in zip(f["ticlDumper/tracksters"]["barycenter_z"].array(library="np"),argmax_idx)]
-
+                    # Load shape Variables
+                    sv = {}
+                    for key in SHAPEVARIABLENAMES:
+                        sv[key] = getMaxShapeVariable(f,key,argmax_idx)
+                        
                     # Split by Cell Type
                     cell_types = [np.array(k[l])/m for k,l,m in zip(f["ticlDumper/tracksters"]["raw_energy_perCellType"].array(), argmax_idx ,tkx_energy)]
 
@@ -128,6 +134,10 @@ class Preprocessor():
                     f1_idx = np.where(tkx_energy==0)    # Exclude Tracksters with no energies
                     f2_idx = np.where(cp_energy<10)        # Exclude CPs with energies less than 10 GeV
                     f_idx = np.union1d(f1_idx,f2_idx)   
+
+                    # Process shape Variables
+                    for key in SHAPEVARIABLENAMES:
+                        sv[key] = filterMaxShapeVariables(sv[key],f_idx)
                     
                     # Write quantities
 
@@ -136,12 +146,10 @@ class Preprocessor():
                     self.cp_tkx_energy_frac += np.delete(tkx_energy/cp_energy,f_idx).tolist() 
                     self.tkx_numtkx += np.delete(tkx_numtkx,f_idx).tolist()
 
-                    bar_x = [i for j, i in enumerate(bar_x) if j not in f_idx]
-                    bar_y = [i for j, i in enumerate(bar_y) if j not in f_idx]
-                    bar_z = [i for j, i in enumerate(bar_z) if j not in f_idx]
-                    self.weighted_bar_x += bar_x
-                    self.weighted_bar_y += bar_y 
-                    self.weighted_bar_z += bar_z
+                    for feature_name in SHAPEVARIABLENAMES:
+                        v = getattr(self, feature_name)
+                        setattr(self, feature_name, v + sv[feature_name])
+
                     # Split Energy per Cell
                     cell_types = [i for j, i in enumerate(cell_types) if j not in f_idx]
                     cell_types = np.array(cell_types)
